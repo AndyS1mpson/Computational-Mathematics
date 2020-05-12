015de3c4a94ece22bf2cb9fd03b0dbb6582e4d18
AndyS1mpson <werter1555rus@mail.ru>
2020-05-12 21:51:44 +0300

    add Gauss KF

--- Integral/Functions.cs
+++ Integral/Functions.cs
@@ -25,5 +25,67 @@ namespace Integral
                             - (3.0 * Math.Pow((lim1 - 3.0 / 2),(8.0 / 3)) / 8 + 9.0 * Math.Pow((lim1 - 3.0 / 2),(5.0 / 3)) / 5 + 27.0 * Math.Pow((lim1 - 3.0 / 2),(2.0 / 3)) / 8);
             return sum;
         }
+        public double IntegralX3(double lim1,double lim2)
+        {
+            double sum;
+            sum = 3.0 * Math.Pow((lim2 - 3.0 / 2),(11.0 / 3)) / 11 + 27.0 * Math.Pow((lim2 - 3.0 / 2),(8.0 / 3)) / 16 + 81.0 * Math.Pow((lim2 - 3.0 / 2),(5.0 / 3)) / 20 
+                            + 81.0 * Math.Pow((lim2 - 3.0 / 2),(2.0 / 3)) / 16
+                                - (3.0 * Math.Pow((lim1 - 3.0 / 2),(11.0 / 3)) / 11 + 27.0 * Math.Pow((lim1 - 3.0 / 2),(8.0 / 3)) / 16 + 81.0 * Math.Pow((lim1 - 3.0 / 2),(5.0 / 3)) / 20 
+                                    + 81.0 * Math.Pow((lim1 - 3.0 / 2),(2.0 / 3)) / 16);
+            return sum;
+        }
+        public double IntegralX4(double lim1,double lim2)
+        {
+            double sum;
+            sum = 3.0 * Math.Pow((lim2 - 3.0 / 2),(14.0 / 3)) / 14 + 18.0 * Math.Pow((lim2 - 3.0 / 2),(11.0 / 3)) / 11 + 81.0 * Math.Pow((lim2 - 3.0 / 2),(8.0 / 3)) / 16 
+                    + 81.0 * Math.Pow((lim2 - 3.0 / 2),(5.0 / 3)) / 10 + 243.0 * Math.Pow((lim2 - 3.0 / 2),(2.0 / 3)) / 32
+                    - (3.0 * Math.Pow((lim1 - 3.0 / 2),(14.0 / 3)) / 14 + 18.0 * Math.Pow((lim1 - 3.0 / 2),(11.0 / 3)) / 11 + 81.0 * Math.Pow((lim1 - 3.0 / 2),(8.0 / 3)) / 16 
+                    + 81.0 * Math.Pow((lim1 - 3.0 / 2),(5.0 / 3)) / 10 + 243.0 * Math.Pow((lim1 - 3.0 / 2),(2.0 / 3)) / 32);
+            return sum;
+        }
+        public double IntegralX5(double lim1,double lim2)
+        {
+            double sum;
+            sum = 3.0 * Math.Pow((lim2 - 3.0 / 2),(17.0 / 3)) / 17 + 45.0 * Math.Pow((lim2 - 3.0 / 2),(14.0 / 3)) / 28 + 135.0 * Math.Pow((lim2 - 3.0 / 2),(11.0 / 3)) / 22 
+                    + 405.0 * Math.Pow((lim2 - 3.0 / 2),(8.0 / 3)) / 32 + 243.0 * Math.Pow((lim2 - 3.0 / 2),(5.0 / 3)) / 16 + 729.0 * Math.Pow((lim2 - 3.0 / 2),(2.0 / 3)) / 64
+                    - (3.0 * Math.Pow((lim1 - 3.0 / 2),(17.0 / 3)) / 17 + 45.0 * Math.Pow((lim1 - 3.0 / 2),(14.0 / 3)) / 28 + 135.0 * Math.Pow((lim1 - 3.0 / 2),(11.0 / 3)) / 22 
+                    + 405.0 * Math.Pow((lim1 - 3.0 / 2),(8.0 / 3)) / 32 + 243.0 * Math.Pow((lim1 - 3.0 / 2),(5.0 / 3)) / 16 + 729.0 * Math.Pow((lim1 - 3.0 / 2),(2.0 / 3)) / 64);
+            return sum;
+        }
+
+        public double[] Kardano(double[] a, double[] x)
+        {
+             double current = a[0]; a[0] = a[2]; a[2] = current;
+             double p = a[1] - a[0] * a[0] / 3.0; // p = b - a^2/3
+             double q = a[2] + 2.0 * a[0] * a[0] * a[0] / 27.0 - a[0] * a[1] / 3.0; // q = c + 2a^3/27 - ab/3
+             double determinant = q * q / 4.0 + p * p * p / 27.0;
+             
+             if (determinant < 0)
+             {
+                 double fi = 0;
+                 if (q < 0) fi = Math.Atan(2.0 * Math.Sqrt(-determinant) / (-q));
+                 if (q > 0) fi = Math.Atan(2.0 * Math.Sqrt(-determinant) / (-q) + Math.PI);
+                 if (q == 0) fi = Math.PI / 2.0;
+
+                 x[0] = 2.0 * Math.Sqrt(-p / 3.0) * Math.Cos(fi / 3.0) - a[0] / 3.0;
+                 x[1] = 2.0 * Math.Sqrt(-p / 3.0) * Math.Cos(fi / 3.0 + 2.0 * Math.PI / 3.0) - a[0] / 3.0;
+                 x[2] = 2.0 * Math.Sqrt(-p / 3.0) * Math.Cos(fi / 3.0 + 4.0 * Math.PI / 3.0) - a[0] / 3.0;
+             }
+             if (determinant > 0)
+             {
+                x[1] = 0;
+                if ((-q) / 2.0 + Math.Pow(determinant, 1.0 / 2.0) < 0)
+                    x[1] += -Math.Pow((q) / 2.0 - Math.Pow(determinant, 1.0 / 2.0), 1.0 / 3.0);
+                else x[1] += Math.Pow((-q) / 2.0 + Math.Pow(determinant, 1.0 / 2.0), 1.0 / 3.0);
+                if (-q / 2.0 - Math.Pow(determinant, 1.0 / 2.0) < 0)
+                    x[1] += -Math.Pow(q / 2.0 + Math.Pow(determinant, 1.0 / 2.0), 1.0 / 3.0) - a[0] / 3.0;
+                else x[1] += Math.Pow(-q / 2.0 - Math.Pow(determinant, 1.0 / 2.0), 1.0 / 3.0) - a[0] / 3.0;
+             }
+             if (determinant == 0){
+                 x[0] = 2 * Math.Pow(-q / 2.0, 1.0 / 3.0) - a[0] / 3.0;
+                 x[1] =  -Math.Pow(-q / 2.0, 1.0 / 3.0) - a[0] / 3.0;
+             }
+            return x;
+        }
     }
 }
--- Integral/Program.cs
+++ Integral/Program.cs
@@ -6,7 +6,7 @@ namespace Integral
     {
         static double f(double x)
             => 2 * Math.Cos(2.5 * x) * Math.Exp(x * 1.0 / 3) + 4 * Math.Sin(3.5 * x) * Math.Exp(-3 * x) + x;
-        static double NewtonKots(double lim1, double lim2, double step)
+        static double NewtonKotsKF(double lim1, double lim2, double step)
         {
             double integral = 0;
             while(lim2 <= b)
@@ -54,6 +54,82 @@ namespace Integral
             return integral;
         }
         
+        static double GaussKF(double lim1,double lim2,double step)
+        {
+            double result = 0;
+            while(lim2 <= b)
+            {
+                double[] moments = new double[6];
+                moments[0] = functions.IntegralX0(lim1,lim2);
+                moments[1] = functions.IntegralX1(lim1,lim2);
+                moments[2] = functions.IntegralX2(lim1,lim2);
+                moments[3] = functions.IntegralX3(lim1,lim2);
+                moments[4] = functions.IntegralX4(lim1,lim2);
+                moments[5] = functions.IntegralX5(lim1,lim2);
+
+
+                // add 3 new nodes
+                double[] nodes = new double[3];
+                nodes[0] = lim1;
+                nodes[1] = lim1 + (lim2-lim1)/2;
+                nodes[2] = lim2;
+                // declare the coeff of system 
+                double[,] matrixArray = new double[3,3];
+                matrixArray[0,0] = moments[0];
+                matrixArray[0,1] = moments[1];
+                matrixArray[0,2] = moments[2];
+                matrixArray[1,0] = moments[1];
+                matrixArray[1,1] = moments[2];
+                matrixArray[1,2] = moments[3];
+                matrixArray[2,0] = moments[2];
+                matrixArray[2,1] = moments[3];
+                matrixArray[2,2] = moments[4];
+                // declare matrix
+                SquareMatrix matrix = new SquareMatrix(matrixArray,3);
+                matrix.LUDecomposition();
+
+                double[] y = new double[3];
+                y[0] = -moments[3];
+                y[1] = -moments[4];
+                y[2] = -moments[5];
+
+                // find the solution
+                var solution = matrix.SolutionSystem(y);
+                double[] solv = new double[solution.GetLength(1)];
+                for(int i = 0;i < solution.GetLength(1);i++)
+                    solv[i] = solution[0,i];
+
+                var p = functions.Kardano(solv,nodes);
+
+                Array.Sort(p);
+
+                matrix._matrix[0,0] = 1;
+                matrix._matrix[0,1] = 1;
+                matrix._matrix[0,2] = 1;
+                matrix._matrix[1,0] = p[0];
+                matrix._matrix[1,1] = p[1];
+                matrix._matrix[1,2] = p[2];
+                matrix._matrix[2,0] = p[0] * p[0];
+                matrix._matrix[2,1] = p[1] * p[1];
+                matrix._matrix[2,2] = p[2] * p[2];
+
+                matrix.LUDecomposition();
+                
+                var s = matrix.SolutionSystem(moments);
+                double[] A = new double[s.GetLength(1)];
+                for(int i = 0;i < A.Length;i++)
+                    A[i] = s[0,i];
+
+                for (int i = 0; i < 3; i++)
+                {
+                    result += A[i] * f(nodes[i]);
+                }
+                lim1 = lim2;
+                lim2 += step;
+
+            }
+            return result;
+        }
         static double a = 1.5;
         static    double b = 3.3;
         static    double alpha = 1/3;
@@ -67,7 +143,7 @@ namespace Integral
             Console.WriteLine("Вариант Ньютона-Котса : " + "\n");
 
             
-            double integral = NewtonKots(a,b,1);
+            double integral = NewtonKotsKF(a,b,1);
             double error = 0.6406;
 
             WriteText("Вариант Ньютона-Котса :",integral,Math.Abs(exactValue - integral),error);
@@ -86,15 +162,15 @@ namespace Integral
             {
                 step *= L;
                 lim2 = lim1 + step;
-                integral = NewtonKots(lim1,lim2,step);
+                integral = NewtonKotsKF(lim1,lim2,step);
 
                 step = step / L;
                 lim2 = lim1 + step;
-                result2 = NewtonKots(lim1,lim2,step);
+                result2 = NewtonKotsKF(lim1,lim2,step);
 
                 step = step / L;
                 lim2 = lim1 + step;
-                result3 = NewtonKots(lim1,lim2,step);
+                result3 = NewtonKotsKF(lim1,lim2,step);
 
                 speed = -Math.Log(Math.Abs((result3 - result2) / (result2 - integral))) / Math.Log(L);
                 System.Console.Out.Write(speed + "\n");
@@ -109,7 +185,7 @@ namespace Integral
             hOpt = (b - a) / hOpt;
             lim1 = a;
             lim2 = a + hOpt;
-            result2 = NewtonKots(lim1, lim2, hOpt);
+            result2 = NewtonKotsKF(lim1, lim2, hOpt);
             error = Math.Abs((result2 - integral) / (Math.Pow(L, degree) - 1)); 
             
             WriteText("Составная ИКФ с оптимальным шагом :",integral,Math.Abs(exactValue - result2),error);
@@ -120,13 +196,39 @@ namespace Integral
             step = (b - a) / 10;
             error = 10;
 
-            Console.WriteLine("скорость сходимости составной КФ Гаусса" + "\n");
+            Console.WriteLine("Скорость сходимости составной КФ Гаусса" + "\n");
 
             while(error > 1e-6)
             {
-                
+                 step *= L;
+                lim2 = a + step;
+                integral = GaussKF(lim1, lim2, step);
+
+                step = step / L;
+                lim2 = a + step;
+                result2 = GaussKF(lim1, lim2, step);
+
+                step = step / L ;
+                lim2 = a + step;
+                result3 = GaussKF(lim1, lim2, step);
+
+                speed = -Math.Log(Math.Abs((result3 - result2) / (result2 - integral))) / Math.Log(L);
+                System.Console.Out.Write(speed + "\n");
+                error = Math.Abs((result2 - integral) / (Math.Pow(L, degree) - 1));
+                integral += (result2 - integral) / (1 - Math.Pow(L, -degree));
             }
 
+            WriteText("Составная ИКФ по Гауссу", integral, Math.Abs(exactValue - integral), error);
+
+             //* * * * * * * * * * * * * * * Cоставная форма Гаусса c оптимальным шагом * * * * * * * * * *
+            hOpt = Math.Ceiling((b - a) / (step * L * Math.Pow((0.00001 / error), 1.0 / degree)));
+            hOpt = (b - a) / hOpt;
+            lim1 = a;
+            lim2 = a + hOpt;
+            result2 = GaussKF(lim1, lim2, hOpt);
+            error = Math.Abs((result2 - integral) / (Math.Pow(L, degree) - 1));
+
+            WriteText("составная ИКФ Гаусса с оптимальным шагом", result2, Math.Abs(exactValue - result2), error);
         }
 
         static void WriteText(string s, double num)
